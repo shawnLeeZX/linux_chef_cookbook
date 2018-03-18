77aaa8fda0e8f86f85a18a69ddaadf9ba88d4f99
Shawn <lishuai918@gmail.com>
2018-03-18 16:49:57 +0800

    Added some comments.

--- files/arch/intel/vdpau.sh
+++ files/arch/intel/vdpau.sh
@@ -1,1 +1,4 @@
+# The default driver would be taken as i915 in ThinkPad X1 Yoga 2nd, while the
+# shared library installed only has va_gl. Set it right.
+# C.f. https://wiki.archlinux.org/index.php/Hardware_video_acceleration
 export VDPAU_DRIVER=va_gl
